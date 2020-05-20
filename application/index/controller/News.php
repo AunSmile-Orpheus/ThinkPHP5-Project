e14b8ad176afb5786ffeeeb1637b780bcfd0171a
AunSmile <39719047+AunSmile-Orpheus@users.noreply.github.com>
2020-05-21 00:15:51 +0800

    Update and rename New.php to News.php

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -11,7 +11,7 @@ header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:*');
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
-class Course
+class News
 {
     //查询课程名，如果没有课程名就是遍历
     public function query()
