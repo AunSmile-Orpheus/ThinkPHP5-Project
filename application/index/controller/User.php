9b14ec4d22c30ea96b86f315587c9d043216dc50
AunSmile <39719047+AunSmile-Orpheus@users.noreply.github.com>
2020-05-20 20:40:32 +0800

    update register api, insert teacher_data to DB.

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -13,59 +13,61 @@ header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
 class User
 {
-	static function rand_str()
-	{
-		$rand_array = "ABCDEFGHIJKLMNOPQRSTOVWSYZabcdefghijklmnopqrstovwsyz1234567890";
-		$ret = '';
-		for ($i = 0; $i != 15; $i = $i + 1)
-			$ret = $ret . $rand_array[rand(0, strlen($rand_array) - 1)];
-		return $ret;
-	}
-	static function get_type($type)
-	{
-		if ($type == '学生')
-			return 0;
-		else if ($type == '老师')
-			return 1;
-		else
-			return 2;
-	}
-
+        static function rand_str()
+        {
+                $rand_array = "ABCDEFGHIJKLMNOPQRSTOVWSYZabcdefghijklmnopqrstovwsyz1234567890";
+                $ret = '';
+                for ($i = 0; $i != 15; $i = $i + 1)
+                        $ret = $ret . $rand_array[rand(0, strlen($rand_array) - 1)];
+                return $ret;
+        }
+        static function get_type($type)
+        {
+                if ($type == '学生')
+                        return 0;
+                else if ($type == '老师')
+                        return 1;
+                else
+                        return 2;
+        }
 	public function login()
-	{
-		$request = new Request;
-		$username = $request->param('username');
-		$password = $request->param('password');
-		$type = User::get_type($request->param('type'));
+        {
+                $request = new Request;
+                $username = $request->param('username');
+                $password = $request->param('password');
+                $type = User::get_type($request->param('type'));
 
-		$user_data = Db::table('tb_user')->where('username', $username)->find();
+                $user_data = Db::table('tb_user')->where('username', $username)->find();
 
-		if ($password != $user_data['password']) {
-			return Web::error_out(1, "密码错误");
-		} else if ($type != $user_data['type']) {
-			return Web::error_out(2, "用户身份不匹配");
+                if ($password != $user_data['password']) {
+                        return Web::error_out(1, "密码错误");
+                } else if ($type != $user_data['type']) {
+                        return Web::error_out(2, "用户身份不匹配");
+                }
+
+                $result = ['code' => 0, 'openid' => $user_data['openid'] ];
+                return json_encode($result);
+        }
+
+        public function register()
+        {
+                $request = new Request;
+                $username = $request->param('username');
+                $password = $request->param('password');
+                $type = User::get_type($request->param('type'));
+                $data = [
+                        'username' => $username,
+                        'password' => $password,
+                        'type' => $type,
+                        'openid' => User::rand_str()
+                ];
+                Db::name('tb_user')->data($data)->insert();
+		//如果是教师账户的注册则在教师表内添加教师数据
+		if($type == 1){
+			Db::name('tb_teacher')->data(['openid' => $data['openid'], 'username' => 'not named']);
 		}
-
-		$result = ['code' => 0, 'openid' => $user_data['openid'] ];
-		return json_encode($result);
-	}
-
-	public function register()
-	{
-		$request = new Request;
-		$username = $request->param('username');
-		$password = $request->param('password');
-		$type = User::get_type($request->param('type'));
-		$data = [
-			'username' => $username,
-			'password' => $password,
-			'type' => $type,
-			'openid' => User::rand_str()
-		];
-		Db::name('tb_user')->data($data)->insert();
-
-		$result = array('code' => 0);
-		return json_encode($result);
-	}
+                $result = array('code' => 0);
+                return json_encode($result);
+        }
 }
 ?>
